786f01993c5beaccf0dbd6c107141d80576913dc
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2018-05-18 22:39:33 -0300

    meshbox.js v0.1
    Estrutura básica para introdução do projeto.

---
+++ javascript/math.js
@@ -0,0 +1,21 @@
+/* Guide Document
+   Author: Cezar Leigos
+   last modify: Cezar Leigos
+   Year: 2018-05-18
+*/
+
+var math = {
+    /*
+       Termo geral de uma pa
+    */
+    PaGeneralTerm : function(a1,n,r) {
+        return a1 + (n -1)*r;
+    },
+
+    /*
+       Soma de todos os termos da pa
+    */
+    PaSumOfTerm : function(a1,an,n) {
+        return ((a1 + an)*n)/2;
+    }
+};
